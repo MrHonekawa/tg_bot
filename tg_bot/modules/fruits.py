af71b708decd38e64e993794778d50e9a98fcc2e
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-31 21:33:20 +0530

    Update fruits.py

--- tg_bot/modules/fruits.py
+++ tg_bot/modules/fruits.py
@@ -5,3 +5,19 @@ from tg_bot import dispatcher
 from telegram import ParseMode, Update, Bot
 from tg_bot.modules.disable import DisableAbleCommandHandler
 from telegram.ext import CallbackContext, run_async
+
+@runs_async
+def fruits3(update: Update, context: CallbackContext):
+  args = context.args
+  update.effective_message.reply_text(random.choice(playful_string.FRUITS3))
+
+@run_async
+def fruits6(update: Update, context: CallbackContext):
+  args = context.args
+  update.effective_messafe.reply_text(random.choice(playful_string.FRUITS6))
+
+FRUITS3_HANDLER = DisableAbleCommandHandler("fruits3", fruits3)
+FRUITS6_HANDLER = DisableAbleCommandHandler("fruits6", fruits6)
+
+dispatcher.add_handler(FRUITS3_HANDLER)
+dispatcher.add_handler(FRUITS6_HANDLER)
