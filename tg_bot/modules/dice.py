6616c887596227d2ce196c129f06ca84ee341b0a
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-31 16:00:30 +0530

    Update dice.py

--- tg_bot/modules/dice.py
+++ tg_bot/modules/dice.py
@@ -1,1 +1,26 @@
+import html
+import random
+import tg_bot.modules.dice_string as dice_string
+from tg_bot import dispatcher
+from telegram import ParseMode, Update, Bot
+from tg_bot.modules.disable import DisableAbleCommandHandler
+from telegram.ext import CallbackContext, run_async
 
+@run_async
+def dice6(update: Update, context: CallbackContext):
+  args = context.args
+  update.effective_message.reply_text(random.choice(dice_string.DICE6)
+
+@run_async
+def dice12(update: Update, context: CallbackContext):
+  args = context.args
+  update.effective_message.reply_text(random.choice(dice_string.DICE12)
+                                      
+@run_async
+def dice18(update: Update, context: CallbackContext):
+  args = context.args
+  update.effective_message.reply_text(random.choice(dice_string.DICE18)
+                                      
+DICE6_HANDLER = DisableAbleCommandHandler("dice6", dice6)
+DICE12_HANDLER = DisableAbleCommandHandler("dice12", dice12)
+DICE18_HANDLER = DisableAbleCommandHandler("dice18", dice18)
