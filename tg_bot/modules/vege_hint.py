663fc6d00cb57d86d4ba933b0171b57561fd7634
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-31 17:22:30 +0530

    Update vege_hint.py

--- tg_bot/modules/vege_hint.py
+++ tg_bot/modules/vege_hint.py
@@ -5,3 +5,9 @@ from tg_bot import dispatcher
 from telegram import ParseMode, Update, Bot
 from tg_bot.modules.disable import DisableAbleCommandHandler
 from telegram.ext import CallbackContext, run_async
+
+@run_async
+def vege3(update: Update, context: CallbackContext):
+  args = context.args
+  update.effective_message.reply_text(random.choice(playful_string.VEGE3))
+  
