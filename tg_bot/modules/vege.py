cf0c949991dee587a9893aab77128a92b96adeba
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-31 17:31:13 +0530

    Update vege.py

--- tg_bot/modules/vege.py
+++ tg_bot/modules/vege.py
@@ -5,3 +5,8 @@ from tg_bot import dispatcher
 from telegram import ParseMode, Update, Bot
 from tg_bot.modules.disable import DisableAbleCommandHandler
 from telegram.ext import CallbackContext, run_async
+
+
+
+
+VEGE3_HANDLER = DisableAbleCommandHandler("vege3", vege3)
