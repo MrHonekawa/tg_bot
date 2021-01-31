46d7371df131021c39e69e95112fa99f4a65add1
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-31 21:18:56 +0530

    Update vege.py

--- tg_bot/modules/vege.py
+++ tg_bot/modules/vege.py
@@ -17,8 +17,8 @@ def vege6(update: Update, context: CallbackContext):
   update.effective_message.reply_text(random.choice(vege.HINT_VEGE6))
 
 
-VEGE3_HANDLER = DisableAbleCommandHandler("vege3", vege3)
-VEGE6_HANDLER = DisableAbleCommandHandler("vege6", vege6)
+VEGE3_HANDLER = DisableAbleCommandHandler("hint_vege3", vege3)
+VEGE6_HANDLER = DisableAbleCommandHandler("hint_vege6", vege6)
 
 dispatcher.add_handler(VEGE3_HANDLER)
 dispatcher.add_handler(VEGE6_HANDLER)
