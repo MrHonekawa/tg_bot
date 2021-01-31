d245badaea3bd1da87d4a46e9a5f8fdd6e656596
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-31 17:25:43 +0530

    Update vege_hint.py

--- tg_bot/modules/vege_hint.py
+++ tg_bot/modules/vege_hint.py
@@ -1,2 +1,2 @@
 HINT_VEGE3 = (
-          "There is hint for playing vegetables 3 game. You must guess one of below vegetables who guess right will be winner. - 1) 
+          "There is hint for playing vegetables 3 game. You must guess one of below vegetables who guess right will be winner. - 1)" 
