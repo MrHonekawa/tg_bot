89e06520e40ca7ff9a43cb750384053fa5b74c3c
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-31 16:58:53 +0530

    Create playful_strings.py

---
+++ tg_bot/modules/playful_strings.py
@@ -0,0 +1,75 @@
+COLOR4 = ( 
+     "Red"
+     "White",
+     "Green",
+     "Yellow",
+     "Peach",
+     "Voilet",
+     "Pink",
+     "Purple"
+
+COLOR6 = (
+      "Blue"
+      "Black",
+      "Golden",
+      "Silver",
+      "Indigo",
+      "Orange",
+      "Brown"
+      "Red",
+      "Green",
+      "Yellow",
+      "Peach",
+      "Purple"
+
+)
+
+VEGE3 = (
+     "Brinjal"
+     "LadyFinger",
+     "Pumbkin",
+     "BootleGaurd",
+     "Spice"
+     "Pea"
+
+VEGE6 = (
+     "Brinjal"
+     "LadyFinger",
+     "Pumbkin",
+     "BitterGuard",
+     "Spice",
+     "Pea",
+     "Tomato"
+     "Potato"
+     "Spinach"
+     "Capsicum"
+     "mould",
+     "AppleGourd",
+
+)
+
+FRUITS3 = (
+         "Apple"
+         "Banana",
+         "Pineapple",
+         "WaterMelon",
+         "Orange",
+         "Grapes",
+         
+)
+
+FRUITS6 = (
+       "Kiwi"
+       "DragonFruit",
+       "Carrot",
+       "Orange",
+       "Apple",
+       "Pineapple",
+       "Avocado",
+       "Mango",
+       "Pomegranet",
+       "Banana",
+       "Coconut"
+       "Papaya"
+       
+)
