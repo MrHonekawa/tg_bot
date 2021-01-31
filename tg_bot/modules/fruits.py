fa140a3612e8114c37dd59f7a45d95891462bb06
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-31 21:26:09 +0530

    Create fruits.py

---
+++ tg_bot/modules/fruits.py
@@ -0,0 +1,7 @@
+import html
+import random
+import tg_bot.modules.playful_string as playful_string
+from tg_bot import dispatcher
+from telegram import ParseMode, Update, Bot
+from tg_bot.modules.disable import DisableAbleCommandHandler
+from telegram.ext import CallbackContext, run_async
