e8a3e0446e2e66fcb598031c8c1ef7f4226f6bbc
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-31 17:32:39 +0530

    Update vege.py

--- tg_bot/modules/vege.py
+++ tg_bot/modules/vege.py
@@ -1,12 +1,15 @@
 import html
 import random
-import tg_bot.modules.playful_string as playful_string
+import tg_bot.modules.vege as vege
 from tg_bot import dispatcher
 from telegram import ParseMode, Update, Bot
 from tg_bot.modules.disable import DisableAbleCommandHandler
 from telegram.ext import CallbackContext, run_async
 
-
+@run_async
+def vege3(update: Update, context: CallbackContext):
+  args = context.args
+  update.effective_message.reply_text(random.choice(vege.VEGE3))
 
 
 VEGE3_HANDLER = DisableAbleCommandHandler("vege3", vege3)
