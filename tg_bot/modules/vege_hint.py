a1f73d9abd23312552f9a327a6a80114ce4a83e7
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-31 20:43:27 +0530

    Update vege_hint.py

--- tg_bot/modules/vege_hint.py
+++ tg_bot/modules/vege_hint.py
@@ -1,6 +1,7 @@
 HINT_VEGE3 = (
-          "There is hint for playing vegetables 3 game. You must guess one of below vegetables who guess right will be winner. --1) Brinjal, 2) LadyFinger, 3) Pumbkin, 4) BottleGaurd, 5) Spice, 6) Pea"
+          "There is hint for playing vegetables game for 3 players. You must guess one of below vegetables who guess right will be winner. --1) Brinjal, 2) LadyFinger, 3) Pumbkin, 4) BottleGaurd, 5) Spice, 6) Pea"
           
 )
 
 HINT_VEGE6 = (
+          "There is hint for playing vegetable game for 6 players. You must guess one of below vegetables who guess right will be winner. --1)Brinjal, 2) LadyFinger, 3) Pumbkin 4) BitterGuard 5) Spice 6) Pea 7) Tomato 8) Potato 9) Spinach 10) Capsicum 11) mould 12) AppleGourd"
