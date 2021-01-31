4404c46df70e0ff98477a789764488d4ce49de93
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-31 21:10:14 +0530

    Update color_hint.py

--- tg_bot/modules/color_hint.py
+++ tg_bot/modules/color_hint.py
@@ -1,7 +1,9 @@
 HINT_COLOR4 = (
            "Color Guessing games played between 4 players. Guess color by getting hint -- 1) Red, 2) White, 3) Green, 4) Yellow, 5) Peach, 6) Voilet, 7) Pink, 8) Purple"
+
 )
 
 HINT_COLOR6 = (
            "Color Guessing games played between 6 players. Guess color by getting hint -- 1) Blue, 2) Black, 3) Golden, 4) Silver, 5) Indigo, 6) Orange, 7) Brown, 8) Red, 9) Green, 10) Yellow, 11) Peach, 12) Purple"
+
 )
