b04f3ddb2ce12cb38e56e9736bb404fd0eb1ae17
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-31 21:44:13 +0530

    Update fruits_hint.py

--- tg_bot/modules/fruits_hint.py
+++ tg_bot/modules/fruits_hint.py
@@ -1,1 +1,7 @@
 HINT_FRUITS3 = (
+            "Guessing Fruits Game played along 3 players. You have to guess fruits name by hints given below -- 1) Apple, 2) Banana, 3) Pineapple, 4) WaterMelon, 5) Orange, 6) Grapes"
+
+)
+
+HINT_FRUITS6 = (
+            "Provided Soon, Hopefully in next update if you want this hint to be revealed faster req to @DragonAssociationSupport"
