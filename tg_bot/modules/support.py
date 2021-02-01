c8d06cfe34ef0c09c7c462a0d3661ae51b9d771b
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-02-01 15:19:11 +0530

    Update support.py

--- tg_bot/modules/support.py
+++ tg_bot/modules/support.py
@@ -9,4 +9,15 @@ from telegram.ext import CallbackContext, run_async
 @run_async
 def support_group(update: Update, context: CallbackContext):
   args = context.args
-  update.effective_message.reply_text(random.choice.SUPPORT)
+  update.effective_message.reply_text(random.choice.SUPPORT))
+
+@run_async
+def update_channel(update: Update, context; CallbackContext):
+  args = context.args
+  update.effective_message.reply_text(random.choice.UPDATE))
+
+SUPPORT_GROUP_HANDLER = DisableAbleCommandHandler("support_group", support_group)
+UPDATE_CHANNEL_HANDLER = DisableAbleCommandHandler("update_channel", update_channel)
+
+dispatcher.add_handler(SUPPORT_GROUP_HANDLER)
+dispatcher.add_handler(UPDATE_CHANNEL_HANDLER)
