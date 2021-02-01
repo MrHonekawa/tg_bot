85864acc0f7904301731c5ca92de2bfb0675435e
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-02-01 09:47:29 +0530

    Update vege.py

--- tg_bot/modules/vege.py
+++ tg_bot/modules/vege.py
@@ -7,18 +7,18 @@ from tg_bot.modules.disable import DisableAbleCommandHandler
 from telegram.ext import CallbackContext, run_async
 
 @run_async
-def vege3(update: Update, context: CallbackContext):
+def hint_vege3(update: Update, context: CallbackContext):
   args = context.args
   update.effective_message.reply_text(random.choice(vege.HINT_VEGE3))
   
 @run_async
-def vege6(update: Update, context: CallbackContext):
+def hint_vege6(update: Update, context: CallbackContext):
   args = context.args
   update.effective_message.reply_text(random.choice(vege.HINT_VEGE6))
 
 
-VEGE3_HANDLER = DisableAbleCommandHandler("hint_vege3", vege3)
-VEGE6_HANDLER = DisableAbleCommandHandler("hint_vege6", vege6)
+HINT_VEGE3_HANDLER = DisableAbleCommandHandler("hint_vege3", hint_vege3)
+HINT_VEGE6_HANDLER = DisableAbleCommandHandler("hint_vege6", hint_vege6)
 
-dispatcher.add_handler(VEGE3_HANDLER)
-dispatcher.add_handler(VEGE6_HANDLER)
+dispatcher.add_handler(HINT_VEGE3_HANDLER)
+dispatcher.add_handler(HINT_VEGE6_HANDLER)
