ee0c4b455a4aec3eb92daf2985c67d633ff44245
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-02-01 10:20:57 +0530

    Update disable.py

--- tg_bot/modules/disable.py
+++ tg_bot/modules/disable.py
@@ -138,7 +138,7 @@ if is_module_loaded(FILENAME):
 
 
     def __stats__():
-        return "{} disabled items, across {} chats.".format(sql.num_disabled(), sql.num_chats())
+        return "• {} disabled items, across {} chats.".format(sql.num_disabled(), sql.num_chats())
 
 
     def __migrate__(old_chat_id, new_chat_id):
