daa92a88fe9b430fd4b5b68c330a787a4c6b47e8
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-02-01 14:18:07 +0530

    Update __init__.py

--- tg_bot/modules/sql/__init__.py
+++ tg_bot/modules/sql/__init__.py
@@ -1,1 +1,16 @@
+from sqlalchmey import create_engine
+from sqlalchmey.ext.declarative import declarative_base
+from sqlalchmey.orm import sessionmaker, scoped_session
 
+from tg_bot import DB_URI
+
+
+def start() -> scoped_session:
+  engine = create.engine(DB_URI, client_encoding="utf8")
+  BASE.metadata.bind = engine
+  BASE.metadata.create_all(engine)
+  return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+BASE = declarative_base()
+SESSION = start()
