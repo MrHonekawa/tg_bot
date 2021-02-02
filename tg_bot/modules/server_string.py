9d7a98c2c8608be0309d66b5792cb71234e5a84c
MrBootsBoots <77720771+MrBootsBoots@users.noreply.github.com>
2021-02-02 11:49:45 +0530

    Delete server_string.py

--- tg_bot/modules/server_string.py
+++
@@ -1,5 +0,0 @@
-SERVER = (
-    "Hello, we are hosted on a private server it is holding a bright future support [Private Server](https://discord.gg/crGUAnmSFD) Too."
-    "Hey, we are glad to tell we are hosted on a private server pls have a support by joining their [discord](https://discord.gg/crGUAnmSFD).",
-    
-    )
