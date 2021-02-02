5d0d4e7abbaebf526ccb748b0055fd115ebc0027
MrBootsBoots <77720771+MrBootsBoots@users.noreply.github.com>
2021-02-02 14:00:33 +0530

    Update dice.py

--- tg_bot/modules/dice.py
+++ tg_bot/modules/dice.py
@@ -1,6 +1,6 @@
 import random, re
 from random import randint
-from telegram import Message, Update, Bot, Users
+from telegram import Message, Update, Bot, User
 from telegram.ext import Filters, MessageHandler, run_async
 
 from tg_bot import dispatcher
