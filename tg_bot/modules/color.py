b7c9bc126f4d59e30ce60c896d80ffefd50a623a
MrBootsBoots <77720771+MrBootsBoots@users.noreply.github.com>
2021-02-02 12:05:56 +0530

    Update color.py

--- tg_bot/modules/color.py
+++ tg_bot/modules/color.py
@@ -6,3 +6,38 @@ from telegram.ext import Filters, MessageHandler, run_async
 
 from tg_bot import dispatcher
 from tg_bot.modules.disable import DisableAbleCommandHandler
+
+COLOR4 = ( 
+     "Red"
+     "White",
+     "Green",
+     "Yellow",
+     "Peach",
+     "Voilet",
+     "Pink",
+     "Purple"
+)
+
+COLOR6 = (
+      "Blue"
+      "Black",
+      "Golden",
+      "Silver",
+      "Indigo",
+      "Orange",
+      "Brown"
+      "Red",
+      "Green",
+      "Yellow",
+      "Peach",
+      "Purple"
+
+)
+
+@run_async
+def color4(bot: Bot, update: Update):
+  update.message.reply_text(random.choice(COLOR4))
+
+@run_async
+def color6(bot: Bot, update: Update):
+  update.message.reply_text(random.choice(COLOR6))
