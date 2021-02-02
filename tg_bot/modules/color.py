8e7e747bff105779dd5a4190637b73406d15092e
MrBootsBoots <77720771+MrBootsBoots@users.noreply.github.com>
2021-02-02 12:00:59 +0530

    Update color.py

--- tg_bot/modules/color.py
+++ tg_bot/modules/color.py
@@ -1,1 +1,8 @@
 import random, re
+from random import randint
+from telegram import Message, Update, Bot, User
+from telegram import MessageEntity
+from telegram.ext import Filters, MessageHandler, run_async
+
+from tg_bot import dispatcher
+from tg_bot.modules.disable import DisableAbleCommandHandler
