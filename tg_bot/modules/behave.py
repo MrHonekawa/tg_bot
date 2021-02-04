f68ccac5a02f9e201a355f49437a0b3d2bbc5baa
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-02-04 09:37:02 +0530

    Update behave.py

--- tg_bot/modules/behave.py
+++ tg_bot/modules/behave.py
@@ -1,1 +1,10 @@
-import 
+# Estella (A telegram bot project)
+# Copyright (C) Dragon Association & Estella Team
+# A python Telegram project
+# Idea gained by ViruZ
+# Python Version 3.6.12
+
+import random
+from telegram.ext import run_async, Filters
+from telegram import Message, Chat, Update, Bot, MessageEntity
+from tg_bot import dispatcher
