8bd073619e12270b175ebedbdbf3c6af53be2344
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-02-04 09:56:51 +0530

    Update grammer_correction.py

--- tg_bot/modules/grammer_correction.py
+++ tg_bot/modules/grammer_correction.py
@@ -1,1 +1,17 @@
+import json
+from pprint import pprint
 
+import requests
+from telegram import Update, Bot
+from telegram.ext import CommandHandler
+
+from tg_bot import dispatcher
+
+# Open API_KEY
+API_KEY = "6ae0c3a0-afdc-4532-a810-82ded0054236"
+URL = "http://services.gingersoftware.com/Ginger/correct/json/GingerTheText"
+
+
+def grammer(bot: Bot, update: Update):
+  if update.effective_message.reply_to_message:
+    msg = update.effective_message.reply_to_message
