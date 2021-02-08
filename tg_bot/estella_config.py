9f49714c3a29b4daa111cce44bcc5684f48a713f
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-02-08 13:47:54 +0530

    Update estella_config.py

--- tg_bot/estella_config.py
+++ tg_bot/estella_config.py
@@ -4,7 +4,7 @@ class Config(object):
 
     # REQUIRED
     API_KEY = "1362939750:AAGWAKfbkzzDQFlmQgPAAF5NXp4oglJgSuQ"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_ID = "1180249738"  # If you dont know, run the bot and do /id in your private chat with it
     OWNER_USERNAME = "I_AM_AN_PRINCES"
 
     # RECOMMENDED
@@ -15,13 +15,13 @@ class Config(object):
     # and killed the bot. Be careful re-enabling it!
     NO_LOAD = []
     WEBHOOK = False
-    URL = None
+    URL = "https://estella-robots.herokuapp.com/"
 
     # OPTIONAL
     SUDO_USERS = '1399308798, 1229419906, 1358136299'  # List of id's (not usernames) for users which have sudo access to the bot.
-    SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    DONATION_LINK = None  # EG, paypal
+    SUPPORT_USERS = '784606914'  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = '1399308798, 1229419906, 1358136299, 784606914'  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    DONATION_LINK = "https://t.me/I_AM_AN_PRINCES"  # EG, paypal
     CERT_PATH = None
     PORT = 5432
     DEL_CMDS = True  # Whether or not you should delete "blue text must click" commands
@@ -29,7 +29,8 @@ class Config(object):
     WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
     BAN_STICKER = ''  # banhammer marie sticker
     ALLOW_EXCL = True  # Allow ! commands as well as /
-    AI_API_KEY = 
+    AI_API_KEY = "3ca02d5dc8795c9073825f2bb1fc02d415c19e44bec07d3fe5e48471185acab3bd44f04dd791ad605b4cbe85394e0a8fad16032d7710ad27de1c41149baeee42"
+    ENV = "ANYTHING"
 
 class Production(Config):
     LOGGER = False
