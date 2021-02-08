e288e547ca2af16ffe70493ea5e7e9dc93c8e565
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-02-08 14:48:42 +0530

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -57,6 +57,7 @@ if ENV:
     WORKERS = int(os.environ.get('WORKERS', 8))
     BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
+    AI_API_KEY = os.environ.get('AI_API_KEY', None)
 
 else:
     from tg_bot.config import Development as Config
