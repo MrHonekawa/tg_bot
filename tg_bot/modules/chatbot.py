aef80716e710637270b4f0f3a43ecb34d20f5024
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-02-08 20:30:13 +0530

    Update chatbot.py

--- tg_bot/modules/chatbot.py
+++ tg_bot/modules/chatbot.py
@@ -12,7 +12,7 @@ from telegram import Message, Chat, User, Update, Bot
 from telegram.ext import CommandHandler, MessageHandler, Filters, run_async
 from telegram.error import BadRequest, Unauthorized, RetryAfter
 
-from tg_bot import dispatcher, API_KEY, OWNER_ID
+from tg_bot import dispatcher, AI_API_KEY, OWNER_ID
 import tg_bot.modules.sql.chatbot_sql as sql
 from tg_bot.modules.helper_funcs.filters import CustomFilters
 
