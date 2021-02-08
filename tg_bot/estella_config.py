723a5543eb99fe93a359ccfa0336d2be9098c727
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-02-08 13:35:04 +0530

    Update estella_config.py

--- tg_bot/estella_config.py
+++ tg_bot/estella_config.py
@@ -5,10 +5,10 @@ class Config(object):
     # REQUIRED
     API_KEY = ""
     OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    OWNER_USERNAME = "I_AM_AN_PRINCES"
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = 'postgres://hixcgohzcigfpn:b81f8eeda4a52905e50cbf65d80c621744e94dcdf1b843ea01cf4bd8afdb4f34@ec2-23-21-236-249.compute-1.amazonaws.com:5432/d6epqfks5skq9a'  # needed for any database modules
     MESSAGE_DUMP = '-1001410340369'  # needed to make sure 'save from' messages persist
     LOAD = []
     # sed has been disabled after the discovery that certain long-running sed commands maxed out cpu usage
@@ -18,12 +18,12 @@ class Config(object):
     URL = None
 
     # OPTIONAL
-    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
+    SUDO_USERS = '1399308798, 1229419906, 1358136299'  # List of id's (not usernames) for users which have sudo access to the bot.
     SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
     WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
-    PORT = 8443
+    PORT = 5432
     DEL_CMDS = True  # Whether or not you should delete "blue text must click" commands
     STRICT_GBAN = True
     WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
